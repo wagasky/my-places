b52db204e1e572acdceb4e7469c5a36f54024ae9
Melissa Wagasky <melissawagasky@gmail.com>
2018-03-24 21:46:10 -0600

    Check form inputs for valid longitude and latitude inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      lat: '',
+      lng: ''
+    }
+  }
   submitForm(e, data) {
     e.preventDefault();
     this.props.saveLocation(data);
   }
+
+  setCoordinates(e) {
+    const { value, name } = e.target
+    this.setState({
+      [name]: value
+    }, () => this.enableSubmit())
+  }
+
+  enableSubmit() {
+    const latCheck = this.validateLatitude(this.state.lat)
+    const lngCheck = this.validateLongitude(this.state.lng)
+  }
+
+  validateLatitude(lat) {
+    const num = parseInt(lat)
+    const result = 
+      (typeof num === 'number' && num <= 90 && num >= -90) ? true : false;
+    return result
+  }
+
+  validateLongitude(lng) {
+    const num = parseInt(lng)
+    const result = 
+      (typeof num === 'number' && num <= 180 && num >= -180) ? true : false;
+    return result
+  }
+
   render() {
     return (
       <form className="form">
@@ -20,13 +55,21 @@ class Form extends Component {
           <input
             ref={(input) => { this.lat = input }}
             type="text"
+            name="lat"
+            placeholder="Enter latitude"
+            onChange={(e) => this.setCoordinates(e)}
           />
         </label>
         <label>
           Lon
           <input
             ref={(input) => { this.lng = input }}
-            type="text"/>
+            type="text"
+            name="lng"
+            placeholder="Enter longitude"
+            onChange={(e) => this.setCoordinates(e)}
+          />
+
         </label>
         <button
           type="submit"
