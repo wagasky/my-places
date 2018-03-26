cafd2b1131817b704557431453da049d281ee0f0
Melissa Wagasky <melissawagasky@gmail.com>
2018-03-25 18:13:22 -0600

    Make map markers clickable

--- src/components/AllMarkers.js
+++ src/components/AllMarkers.js
@@ -4,6 +4,11 @@ import { connect } from 'react-redux';
 import MapMarker from './Marker'
 
 class AllMarkers extends Component {
+
+  createPolygon(e) {
+    console.log('click', e)
+  }
+
   render() {
     const markerArray = this.props.locations.map((marker, i) => {
       return (
@@ -11,6 +16,7 @@ class AllMarkers extends Component {
           key={i}
           location={[+marker.lat, +marker.lng]}
           name={marker.name}
+          handleClick={e => this.createPolygon(e)}
         />
       )
     })
--- src/components/Marker.js
+++ src/components/Marker.js
@@ -5,17 +5,24 @@ import tealdot from '../imgs/tealdot.svg'
 
 class MapMarker extends Component {
 
+  handleClick() {
+    console.log('click')
+  }
+
   render() {
     let icon = L.icon({
       iconUrl: tealdot,
       iconSize: [15, 15]
     });
 
+    const handleClick = this.props.handleClick
+    
     return (
       <div className="marker-container">
           <Marker
             position={this.props.location}
             icon={icon}
+            onClick={handleClick}
           >
             <Tooltip
               sticky
